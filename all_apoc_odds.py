25cfdad9eaef52cc1894d18de1144f8e99ce5861
Doggie Licc <miguelinchavarria@gmail.com>
2023-06-14 23:18:10 -0400

    new simulation!

---
+++ all_apoc_odds.py
@@ -0,0 +1,44 @@
+import utils.role_buckets
+from utils.presets_rolelists import AllAny
+from utils.roles import Doomsayer
+
+import random
+
+# Simulate the odds of Doomsayer winning N1 by randomly guessing in AllAny
+
+if __name__ == '__main__':
+
+    while True:
+        num_gens_str = input('Type in the amount of lists to generate: (default:1000) ')
+        num_gens = None
+
+        if not num_gens_str:
+            num_gens = 1000
+
+        else:
+            if num_gens_str.isnumeric():
+                num_gens = int(num_gens_str)
+
+        if num_gens:
+            break
+
+    apoc_roles = utils.role_buckets.APOCALYPSE_ROLES
+
+    success_count = 0
+
+    print('Generating...')
+
+    for i in range(num_gens):
+
+        roles = AllAny.generate_roles()
+
+        apocs = [r for r in roles if r in apoc_roles]
+
+        if len(apocs) == 4:
+            success_count += 1
+
+        if num_gens % 1000 == 0:
+            print(f'generated {i}/{num_gens}')
+
+
+    print(f'{success_count}/{num_gens} ({(success_count / num_gens): .2f}%)')
