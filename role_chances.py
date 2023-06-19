399326ffc21a3b64e42fa6751e7b20eb95ec3c21
Doggie Licc <miguelinchavarria@gmail.com>
2023-06-18 23:44:22 -0400

    Add opposing faction check setting

--- role_chances.py
+++ role_chances.py
@@ -1,9 +1,10 @@
-from utils.presets_rolelists import AllAny, Classic
+from utils.presets_rolelists import AllAny, Classic, Ranked1
+from utils.classes import check_list_for_opposing_factions
 from collections import Counter
 
 
 if __name__ == '__main__':
-    role_lists = [AllAny, Classic]
+    role_lists = [AllAny, Classic, Ranked1]
 
     print('Available rolelists: ' + ', '.join([f"'{rl.name}'" for rl in role_lists]))
 
@@ -20,6 +21,24 @@ if __name__ == '__main__':
     print(f'You selected: {rolelist.name}\n')
 
     print('\n'.join([f'  {r.name}' for r in rolelist.roles]))
+
+    while True:
+        check_opposing_facs_check_str = input('Check generated lists for opposing factions? (y/N): ')
+
+        if not check_opposing_facs_check_str:
+            check_opposing_facs = False
+            break
+
+        if check_opposing_facs_check_str.lower() == 'y':
+            check_opposing_facs = True
+            break
+
+        if check_opposing_facs_check_str.lower() == 'n':
+            check_opposing_facs = False
+            break
+
+    if check_opposing_facs:
+        print('Checking lists for opposing factions!')
 
     while True:
         num_gens_str = input('Type in the amount of lists to generate (default:10000): ')
@@ -40,9 +59,28 @@ if __name__ == '__main__':
     role_counter = Counter()
 
     print('Generating...')
-    for _ in range(num_gens):
+
+    successful_gens = 0
+
+    while successful_gens < num_gens:
         roles = rolelist.generate_roles()
+
+        if check_opposing_facs:
+            opposing_facs = check_list_for_opposing_factions(roles)
+
+            if not opposing_facs:
+                print(f'Caught generated list with no opposing factions, retrying...: {roles}')
+                continue
+
+        successful_gens += 1
+
+        if successful_gens % 1000 == 0:
+            print(f'generated {successful_gens}/{num_gens}')
+
         unique_roles = set(roles)
         role_counter.update(unique_roles)
 
-    print(role_counter)
+    print('Done!\n\n')
+
+    for k, v in role_counter.most_common():
+        print(f'{k.name}: ({v}/{num_gens}) {v*100/num_gens: .2f}%')
