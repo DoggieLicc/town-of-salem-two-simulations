4d99b5d9b649c84857163a36a856ebc8f3b5c005
Doggie Licc <miguelinchavarria@gmail.com>
2023-07-07 20:46:28 -0400

    Minor optimizations

--- utils/classes.py
+++ utils/classes.py
@@ -74,14 +74,14 @@ def check_role_limit(generated_roles: List[Role], role: Role) -> bool:
     if not role.limit or len(generated_roles) < role.limit:
         return True
 
-    num_roles = len([r for r in generated_roles if r == role])
+    num_roles = sum(1 for r in generated_roles if r == role)
 
     return num_roles + 1 <= role.limit
 
 
 def check_rolebucket_limit(generated_roles: List[Role], role: Role) -> bool:
     if role in RoleBuckets.COVEN_ROLES:
-        num_roles = len([r for r in generated_roles if r in RoleBuckets.COVEN_ROLES])
+        num_roles = sum(1 for r in generated_roles if r in RoleBuckets.COVEN_ROLES)
 
         return num_roles + 1 <= RoleBuckets.RandomCoven.limit
 
@@ -100,18 +100,19 @@ def get_valid_roles(generated_roles: List[Role], possible_roles: Set[Role], bann
 
 
 def check_list_for_opposing_factions(role_list: List[Role]) -> bool:
-    townies = [r for r in role_list if r in RoleBuckets.TOWN_ROLES]
-    coven = [r for r in role_list if r in RoleBuckets.COVEN_ROLES]
+    role_set = set(role_list)
+    townies = role_set.intersection(RoleBuckets.TOWN_ROLES)
+    coven = role_set.intersection(RoleBuckets.COVEN_ROLES)
 
     if townies and coven:
         return True
 
-    apocalypse = [r for r in role_list if r in RoleBuckets.APOCALYPSE_ROLES]
+    apocalypse = role_set.intersection(RoleBuckets.APOCALYPSE_ROLES)
 
     if (townies and apocalypse) or (coven and apocalypse):
         return True
 
-    unique_neutral_killings = set([r for r in role_list if r in RoleBuckets.NeutralKilling.expand_possible_roles()])
+    unique_neutral_killings = role_set.intersection(RoleBuckets.NeutralKilling.expand_possible_roles())
 
     if unique_neutral_killings:
         return bool(townies or coven or apocalypse or len(unique_neutral_killings))
